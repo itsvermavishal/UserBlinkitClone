ac6805c562957f0c5f99d83b165de7e35d739ea5
Vishal Verma <vishalpbh022@gmail.com>
2025-12-22 21:24:29 +0530

    Compleated notification service

--- app/src/main/java/com/example/userblinkitclone/NotificationService.kt
+++ app/src/main/java/com/example/userblinkitclone/NotificationService.kt
@@ -2,8 +2,11 @@ package com.example.userblinkitclone
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.example.userblinkitclone.activity.UsersMainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import kotlin.random.Random
@@ -21,11 +24,13 @@ class NotificationService: FirebaseMessagingService() {
 
         val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         manager.createNotificationChannel(channel)
-
+        val pendingIntent = PendingIntent.getActivity(this, 0, Intent(this, UsersMainActivity::class.java), PendingIntent.FLAG_IMMUTABLE)
         val notification = NotificationCompat.Builder(this, channelId)
             .setContentTitle(message.data["title"])
             .setContentText(message.data["message"])
             .setSmallIcon(R.drawable.app_icon)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .build()
         manager.notify(Random.nextInt(), notification)
     }
